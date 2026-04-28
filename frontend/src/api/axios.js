2f0769d472007136938c0fa59c3f5606aaf475a8
vatssahil01 <vatssahil003@gmail.com>
2026-04-28 14:51:47 +0530

    changes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1',
-    baseURL: import.meta.env.VITE_API_URL || 'https://book-store-e-commerce-9dla.onrender.com/api/v1/books',
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api/v1',
     headers: {
         'Content-Type': 'application/json',
     },
